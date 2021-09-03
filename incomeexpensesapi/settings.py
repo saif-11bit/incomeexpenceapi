f06ea2f05dac0d4bbe234177e4c403c81a45e1a1
Saif Ahmed <saifmdco@gmail.com>
2021-09-03 21:14:47 +0530

    test

--- incomeexpensesapi/settings.py
+++ incomeexpensesapi/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 env = environ.Env()
 environ.Env.read_env()
 
